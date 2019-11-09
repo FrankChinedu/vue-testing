031162ad1d63e09e6ff275656a63812438bd1428
obi chinedu frank <frankieetchy@gmail.com>
2019-11-09 19:00:29 +0100

    third lesson done

--- specs/github.spec.js
+++ specs/github.spec.js
@@ -2,5 +2,28 @@ import GithubCard from '@/github-card'
 import { mount } from '@vue/test-utils'
 
 describe('methods', () => {
+  test('composeUrl', () => {
+    const {composeUrl} = GithubCard.methods
+    expect(composeUrl(123)).toBe('https://api.github.com/users/123')
+    expect(composeUrl('some')).toBe('https://api.github.com/users/some')
+  });
 
-})
+  test('fetchData', async () => {
+    const jsonMock = jest.fn().mockResolvedValue('GITHUB DATA');
+
+    window.fetch = jest.fn().mockResolvedValue({
+      json: jsonMock
+    });
+
+    const wrapper = mount(GithubCard, {
+      methods: {
+        composeUrl: () => 'url',
+      },
+    });
+    await wrapper.vm.fetchData();
+
+    expect(window.fetch).toHaveBeenCalledWith('url');
+    expect(jsonMock).toHaveBeenCalled();
+    expect(wrapper.vm.data).toBe('GITHUB DATA');
+  });
+});
